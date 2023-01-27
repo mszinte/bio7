5265ed35610fad38269049c2d2b705e0d4db8bce
mszinte <mszinte@invibe.nohost.me>
2023-01-27 10:25:05 +0100

    change utils to create function for mode and group

--- analysis_code/preproc/bids/beh_events.py
+++ analysis_code/preproc/bids/beh_events.py
@@ -15,13 +15,13 @@ new freesurfer segmentation files
 -----------------------------------------------------------------------------------------
 To run:
 1. cd to function
->> ~/projects/bio7/analysis_code/preproc/bids/
+>> cd ~/projects/bio7/analysis_code/preproc/bids/
 2. run python command
 python beh_events.py [main directory] [project name] [subject]
 -----------------------------------------------------------------------------------------
 Exemple:
 python beh_events.py /scratch/jstellmann/data/bio7 
-                     ~/projects/bio7/experiment_code/data/retin sub-8761
+                     ~/projects/bio7/experiment_code/retin/data sub-8761
 ------------------------------------------------------------------------------------------
 Written by Martin Szinte (mail@martinszinte.net)
 ----------------------------------------------------------------------------------------
@@ -31,7 +31,8 @@ Written by Martin Szinte (mail@martinszinte.net)
 import os
 import sys 
 import glob
-import ipdb
+sys.path.append("{}/../../utils".format(os.getcwd()))
+from permission import change_file_mod, change_file_group
 
 # Get inputs
 bids_dir = sys.argv[1]
@@ -48,4 +49,7 @@ os.system(repo2sourcedata_cmd)
 sourcedata_bids_cmd= "rsync -avuz {bids}/sourcedata/beh/retin/{sub}/ses-02/func/ {bids}/{sub}/ses-02/func/".format(
     bids=bids_dir, sub=subject)
 print(sourcedata_bids_cmd)
-os.system(sourcedata_bids_cmd)
+
+# Change file mode and group
+change_file_mod(bids_dir)
+change_file_group(bids_dir)
