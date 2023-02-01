e26bc0f7269604db2319596ebf79c2cc11dad926
ptilsley <you@example.com>
2023-02-01 19:11:28 +0100

    modified deface code, needs help fsl

--- analysis_code/preproc/bids/deface_sbatch.py
+++ analysis_code/preproc/bids/deface_sbatch.py
@@ -3,84 +3,204 @@
 deface_sbatch.py
 -----------------------------------------------------------------------------------------
 Goal of the script:
-Deface T1w images
+Deface UNI and INV images
 -----------------------------------------------------------------------------------------
 Input(s):
-sys.argv[1]: main project directory
-sys.argv[2]: project name (correspond to directory)
-sys.argv[3]: subject (e.g. sub-001)
-sys.argv[4]: overwrite images (0 = no, 1 = yes)
-sys.argv[5]: server job or not (1 = server, 0 = terminal)
+sys.argv[1]: mesocentre project data directory (e.g. /scratch/jstellmann/data/bio7)
+sys.argv[2]: subject (e.g., sub-8761 or 8761, all)
+sys.argv[3]: run with slurm server (0 = no: use terminal, 1 = yes: use server)
 -----------------------------------------------------------------------------------------
 Output(s):
 Defaced images
 -----------------------------------------------------------------------------------------
 To run:
-1. cd to function
->> cd /home/mszinte/projects/stereo_prf/mri_analysis/
-2. run python command
-python preproc/deface_sbatch.py [main directory] [project name] [subject num] 
-                                [overwrite] [server_or_not] 
+cd /home/mszinte/projects/stereo_prf/mri_analysis/preproc/
+python deface_sbatch.py [meso_proj_dir] [subject_number] [slurm]
 -----------------------------------------------------------------------------------------
-Exemple:
-python analysis_code/preproc/deface_sbatch.py /scratch/mszinte/data stereo_prf sub-01 0 0
+Example:
+python deface_sbatch.py /scratch/jstellmann/data/bio7 sub-8761 1
 -----------------------------------------------------------------------------------------
-Written by Martin Szinte (martin.szinte@gmail.com)
+Written by Martin Szinte (martin.szinte@gmail.com), Penelope Tilsley
 -----------------------------------------------------------------------------------------
 """
 
 # imports modules
 import sys
 import os
+import subprocess
 import pdb
 deb = pdb.set_trace
 
-# inputs
-main_dir = sys.argv[1]
-project_dir = sys.argv[2]
-subject = sys.argv[3]
-ovewrite_in = int(sys.argv[4])
-server_in = int(sys.argv[5])
+# Defining Help Messages
+#-----------------------
+import argparse
+parser = argparse.ArgumentParser(
+    description = 'Deface MP2RAGE UNI and INV images using pydeface',
+    epilog = '')
+parser.add_argument('[meso_proj_dir]', help='path to project data directory on mesocentre. e.g., /scratch/{user}/data/{project}')
+parser.add_argument('[subject_number]', help='subject to analyse. OPTIONS: treating specific subjects, accepted formats: 8762, sub-8762, 876-2')
+parser.add_argument('[slurm]', help='user slurm server 0 = no, 1 = yes')
+parser.parse_args()
+
+## Check_install
+#---------------
+rc = subprocess.call(['which', 'pydeface'])
+if rc == 0:
+    print('pydeface installed!')
+else:
+    try: 
+        print('Trying pip install pydeface')
+        subprocess.call(['pip', 'install', 'pydeface'])
+    except:
+        print('Didnt manage to install?')
+    else:
+        print('pydeface installed') 
+
+
+# Get inputs
+# ----------
+meso_proj_dir = sys.argv[1]
+subject_level = sys.argv[2]
+server_in = int(sys.argv[3])
+
+# Check inputs
+# ------------
+if os.path.isdir(meso_proj_dir):
+    print("meso project input OK")
+    if meso_proj_dir.endswith("/"):
+        #True
+        meso_proj_dir = meso_proj_dir[:-1]
+    else:
+        #True
+        pass            
+else:
+    print("meso project input doesnt exist")
+    exit()
+    
+# Getting subject number
+#----------------------- 
+subj = subject_level.split("-")[-1]
+
+if len(subj)<4:
+    #Account for input subject number without dashes
+    subj_dcmid = subject_level
+    subject = subject_level.replace("-","")
+else:
+    #Account for input subject number as label or full number
+    subj_dcmid = subj[0:3] + '-' + subj[3:] 
+    subject = subj    
+    
+# Define home + project
+#-----------------------
+meso_home_dir = os.path.expanduser('~')
+projname = meso_proj_dir.split('/')[-1]
+
+# Define log directory
+#---------------------
+meso_log_dir = "{}/sourcedata/logs/anat/".format(meso_proj_dir)  
+os.makedirs(meso_log_dir,exist_ok=True)
+
+## Defining SLURM
+#----------------
 hour_proc = 4
 nb_procs = 8
-log_dir = "{}/{}/derivatives/pp_data/logs".format(main_dir, project_dir, subject)
-try: os.makedirs(log_dir)
-except: pass
 
 # define SLURM cmd
 slurm_cmd = """\
 #!/bin/bash
 #SBATCH -p skylake
-#SBATCH -A b161
+#SBATCH -A a306
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task={nb_procs}
 #SBATCH --time={hour_proc}:00:00
-#SBATCH -e {log_dir}/{subject}_deface_%N_%j_%a.err
-#SBATCH -o {log_dir}/{subject}_deface_%N_%j_%a.out
-#SBATCH -J {subject}_deface\n\n""".format(nb_procs=nb_procs, hour_proc=hour_proc, subject=subject, log_dir=log_dir)
+#SBATCH -e {meso_log_dir}/{subject}_deface_%N_%j_%a.err
+#SBATCH -o {meso_log_dir}/{subject}_deface_%N_%j_%a.out
+#SBATCH -J {subject}_deface\n\n""".format(nb_procs=nb_procs, hour_proc=hour_proc, subject=subject, meso_log_dir=meso_log_dir)
 
-# get files
-session = 'ses-02'
-t1w_filename = "{}/{}/sourcedata/dcm2niix/{}/{}/anat/{}_{}_T1w.nii.gz".format(main_dir,project_dir,subject,session,subject,session)
 
-# sh folder & file
-sh_folder = "{}/{}/derivatives/pp_data/jobs".format(main_dir, project_dir, subject)
-try: os.makedirs(sh_folder)
-except: pass
-sh_file = "{}/{}_deface.sh".format(sh_folder,subject)
+## Finding T1 images
+#-------------------
+subj_dir = "{}/sub-{}".format(meso_proj_dir,subject)
+sessions = [ses for ses in os.listdir(subj_dir) if "ses" in ses]
 
-of = open(sh_file, 'w')
-if server_in: of.write(slurm_cmd)
-if ovewrite_in == 1: deface_cmd = "pydeface {fn} --outfile {fn} --force --verbose\n".format(fn = t1w_filename)
-else: deface_cmd = "pydeface {fn} --verbose\n".format(fn = t1w_filename)
-of.write(deface_cmd)    
-of.close()
+for it in sessions:
+    tmp_sess_dir = os.path.join(subj_dir, it)
+    tmp_anat_dir = os.path.join(tmp_sess_dir, "anat")
+    
+    if os.path.exists(tmp_anat_dir):
+        anat_scans = [scn for scn in os.listdir(tmp_anat_dir) if scn.endswith("FLAWS.nii.gz") or scn.endswith("MP2RAGE.nii.gz")]
+        uni_image = [uni for uni in os.listdir(tmp_anat_dir) if "UNI" in uni and uni.endswith("MP2RAGE.nii.gz")]
+        apply_to_images = list(set(anat_scans) - set(uni_image))
+    
+    
+        # Test for images
+        #----------------
+        if len(uni_image) == 0 and len(apply_to_images) == 0:
+            print("no T1 images to deface in " + tmp_anat_dir) 
+            deface = 'no'
+            applyto = 'no'
 
-print(sh_file)
-# Run or submit jobs
-if server_in:
-    os.chdir(log_dir)
-    print("Submitting {} to queue".format(sh_file))
-    os.system("sbatch {}".format(sh_file))
-else:
-    os.system("sh {}".format(sh_file))
+        elif len(uni_image) == 1 and len(apply_to_images) == 0:
+            print("only T1 UNI image to deface in " + tmp_anat_dir)
+
+            deface = 'yes'
+            uni_filepath = os.path.join(tmp_anat_dir, uni_image[0])
+
+            applyto = 'no'
+
+            deface_cmd = "pydeface {uni} --cost normmi --force --verbose\n".format(uni = uni_filepath)
+
+        elif len(uni_image) == 1 and len(apply_to_images) > 0:
+            print("T1 UNI image to deface in " + tmp_anat_dir)
+            print("and will apply deface to " + str(apply_to_images))
+
+            deface = 'yes'
+            uni_filepath = os.path.join(tmp_anat_dir, uni_image[0])
+
+            applyto = 'yes'
+            applypath = [tmp_anat_dir + '/' + img for img in apply_to_images] 
+            apply_filepath = ' '.join(applypath)
+
+            deface_cmd = "pydeface {uni} --cost normmi --applyto {apply} --force --verbose\n".format(uni = uni_image, apply = apply_filepath)
+
+        elif len(uni_image) == 0 and len(apply_to_images) > 0:
+            print("please check sub-" + tmp_anat_dir + " no UNI but other anat images?: " + str(apply_to_images))
+            deface = 'no'
+            applyto = 'no'
+
+        elif len(uni_image) > 0:
+            print("please check sub-" + tmp_anat_dir + " more than one UNI image? :" + str(uni_image))
+            deface = 'no'
+            applyto = 'no'
+        else: 
+            deface = 'no'
+            applyto = 'no'
+
+
+        # Create shell if defacing needed
+        #--------------------------------
+        if deface == 'no':
+            pass
+        elif deface == 'yes':
+
+            # sh folder & file
+            sh_folder = "{}/code/shell_jobs".format(meso_proj_dir)
+            os.makedirs(sh_folder,exist_ok=True)
+            sh_file = "{}/sub-{}_{}_deface.sh".format(sh_folder,subject, it)
+
+            of = open(sh_file, 'w')
+            if server_in: of.write(slurm_cmd)
+            of.write(deface_cmd)    
+            of.close()
+
+            print("Created " + sh_file + " with following commands: ")
+            shpr = open(sh_file, 'r').read()
+            print(shpr)
+
+            # Run or submit jobs
+            if server_in:
+                os.chdir(meso_log_dir)
+                print("Submitting {} to queue".format(sh_file))
+                os.system("sbatch {}".format(sh_file))
+            else:
+                os.system("sh {}".format(sh_file))
