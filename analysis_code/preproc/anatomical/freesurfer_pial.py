c1cb7b8cafb6c72d4e7ec125044a6ea953091ed2
ptilsley <you@example.com>
2023-03-08 16:08:58 +0100

    updated freesurfer pial codes

--- analysis_code/preproc/anatomical/freesurfer_pial.py
+++ analysis_code/preproc/anatomical/freesurfer_pial.py
@@ -17,10 +17,10 @@ To run:
 1. cd to function
 >> ~/projects/bio7/analysis_code/preproc/anatomical/
 2. run python command
-python freesurfer_pial.py [main directory] [project name] [subject]
+python freesurfer_pial.py [main directory] [subject]
 -----------------------------------------------------------------------------------------
 Exemple:
-python freesurfer_pial.py /scratch/jstellmann/data/ bio7 sub-8764
+python freesurfer_pial.py /scratch/jstellmann/data/bio7 sub-8764
 -----------------------------------------------------------------------------------------
 Written by Martin Szinte (mail@martinszinte.net)
 -----------------------------------------------------------------------------------------
@@ -33,20 +33,26 @@ import json
 
 # inputs
 main_dir = sys.argv[1]
-project_dir = sys.argv[2]
-subject = sys.argv[3]
+subject = sys.argv[2]
 
 # Define cluster/server specific parameters
 cluster_name = 'skylake'
 hour_proc = 20
 nb_procs = 8
 memory_val = 48
-proj_name = 'a306'
+
+proj = main_dir.split('/')[-1]
+
+if proj == "bio7": 
+    proj_name = 'a306'
+    group = 306
+else: pass
+
 
 # define directory and freesurfer licence
-log_dir = "{}/{}/derivatives/freesurfer_pial/log_outputs".format(main_dir,project_dir)
-job_dir = "{}/{}/derivatives/freesurfer_pial/jobs".format(main_dir,project_dir)
-fs_dir = "{}{}/derivatives/fmriprep/freesurfer/".format(main_dir, project_dir)
+log_dir = "{}/derivatives/freesurfer_pial/log_outputs".format(main_dir)
+job_dir = "{}/derivatives/freesurfer_pial/jobs".format(main_dir)
+fs_dir = "{}/derivatives/fmriprep/freesurfer/".format(main_dir)
 fs_licence = '/scratch/jstellmann/data/freesurfer/license.txt'
 os.makedirs(log_dir, exist_ok=True)
 os.makedirs(job_dir, exist_ok=True)
@@ -66,17 +72,24 @@ slurm_cmd = """\
 #SBATCH -J {subject}_freesurfer-pial\n\n""".format( nb_procs=nb_procs, subject=subject, memory_val = memory_val, 
                                                     hour_proc=hour_proc, log_dir=log_dir, proj_name=proj_name)
 
+# define permission cmd
+chmod_cmd = "chmod -Rf 771 {main_dir}\n".format(main_dir=main_dir)
+chgrp_cmd = "chgrp -Rf {group} {main_dir}\n".format(main_dir=main_dir, group=group)
+
+
 # define freesurfer command
 freesurfer_cmd = """\
+export FREESURFER_HOME={}/code/freesurfer
 export SUBJECTS_DIR={}\n\
 export FS_LICENSE={}\n\
-recon-all -autorecon-pial -subjid {}""".format(fs_dir, fs_licence, subject)
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+recon-all -autorecon-pial -subjid {} -no-isrunning""".format(main_dir, fs_dir, fs_licence, subject)
 
 # create sh folder and file
 sh_dir = "{}/{}_freesurfer-pial.sh".format(job_dir, subject)
 
 of = open(sh_dir, 'w')
-of.write("{}{}".format(slurm_cmd,freesurfer_cmd))
+of.write("{}{}{}{}".format(slurm_cmd, chmod_cmd, chgrp_cmd, freesurfer_cmd, chmod_cmd, chgrp_cmd))
 of.close()
 
 # submit jobs
